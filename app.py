b768316c2d5fefa95725d1612ae8ee223347376d
louis-ngugi <louisngugi858@gmail.com>
2025-03-31 22:51:44 +0300

    Configure Flask to use SQLite database

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_migrate import Migrate
 from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///superheroes.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 if __name__ == "__main__":
     app.run(debug=True)
